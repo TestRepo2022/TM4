655e9e5be7a932b76596dfee475ff207b61b8dcb
shivendraprataprathor@gmail.com <shivendraprataprathor@gmail.com>
2022-09-22 22:41:48 +0530

    Add project files.

---
+++ TM4/BankingSavingCustomer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM4
+{
+    class BankingSavingCustomer
+    {
+        public string AcNo { get; }
+
+
+        public string Name;
+        private decimal _Amount;
+
+        public BankingSavingCustomer(string AcNo, string Name, decimal Amount)
+        {
+            this.AcNo = AcNo;
+            this.Name = Name;
+            this._Amount = Amount;
+        }
+
+        //public string AcNo
+        //{
+        //    get { return _AcNo; }
+        //}
+
+        public decimal Amount
+        {
+            get { return _Amount; }
+            set
+            {
+                if (value >= 1000) {
+                    _Amount = value;
+                }
+                else
+                {
+                    Console.WriteLine(" Transaction failed,Insufficient amount !");
+                }
+            }
+        }
+    }
+}
---
+++ TM4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee E = new Employee();
+            //E.Name = "Deepak";
+            //E.Id = 1011;
+            //E.Email = "deepak8999@gmail.com";
+            //E.Salary = 12000;
+          
+
+            E["id"] = 1001;
+            E["name"] = "Rakesh";
+            E["email"] = "rakesh55@gmail.com";
+            E[3] = 13000;
+
+            Console.WriteLine(E[0]);
+            Console.WriteLine(E[1]);
+            Console.WriteLine(E[2]);
+            Console.WriteLine(E["salary"]);
+
+
+           
+
+
+            //BankingSavingCustomer customer = new BankingSavingCustomer("23100922112", "Rakesh Sharma", 20000);
+
+            //Console.WriteLine("----------Customer details---------------------");
+            //Console.WriteLine("Customer Account Number: "+customer.AcNo);
+            //Console.WriteLine("Customer Name: "+customer.Name);
+            //Console.WriteLine("Customer Amount: "+customer.Amount);
+
+            ////customer.AcNo = "6666";
+            //customer.Amount -= 16000;
+
+
+            //Console.WriteLine("----------Customer details after transaction---------------------");
+            //Console.WriteLine("Customer Account Number: " + customer.AcNo);
+            //Console.WriteLine("Customer Name: " + customer.Name);
+            //Console.WriteLine("Customer Amount: " + customer.Amount);
+            Console.Read();
+        }
+    }
+}
